25b2eedc818d9687c07f36e6de270b376145eef1
Daijiro SUENAGA <info@awks.jp>
2014-08-24 18:04:47 +0900

    Revert "ja -> ja-jp"
    This reverts commit 422090f140679539cd32e1a9c9d7a42351b144f1.

--- js/script.js
+++ js/script.js
@@ -311,15 +311,16 @@ $(function() {
   function setupL20n() {
     // ブラウザの言語設定
     var lang = navigator.language.toLowerCase();
-    lang = 'ja' === lang ? 'ja-jp' : lang;
     ctx = L20n.getContext();
     // デフォルト設定と可能言語
-    ctx.registerLocales('ja-jp', ['en-us']);
+    ctx.registerLocales('ja', ['en-us']);
     ctx.linkResource('./locales/' + lang + '/website.l20n');
     ctx.requestLocales();
+    /*
     ctx.localize(['about'], function(l10n) {
       console.log(l10n.entities.about.value);
     });
+    */
   }
 
   // ローカルストレージ（エリア名）
